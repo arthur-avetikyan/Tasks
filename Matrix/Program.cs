215abc10199bd05e2fef7ca4c70e19835246921d
arthur-avetikyan <arthuravetikyan@gmail.com>
2020-07-08 11:31:54 +0400

    print function

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -10,16 +10,24 @@ namespace Matrix
     {
         static void Main(string[] args)
         {
-            int[,] lMatrix = new int[4, 4] { { 61, 5, 66, 8 }, { 17, 8, 92, 27 }, { 27, 32, 43, 5 }, { 64, 32, 64, 85 } };
+            int[,] lNumberMatrix = new int[4, 4] { { 61, 5, 66, 8 }, { 17, 8, 92, 27 }, { 27, 32, 43, 5 }, { 64, 32, 64, 85 } };
+            string[,] lTextMatrix = new string[4, 4] { { "61", "5", "66", "8" }, { "17", "8", "92", "27" }, { "27", "32", "43", "5" }, { "64", "32", "64", "85" } };
 
-            HashSet<int> lDublicates = LinearSearch<int>.FindDublicatesInMatrix(lMatrix);
+            HashSet<int> lDublicateNumbers = LinearSearch<int>.FindDublicatesInMatrix(lNumberMatrix);
+            HashSet<string> lDublicateText = LinearSearch<string>.FindDublicatesInMatrix(lTextMatrix);
 
-            foreach (int item in lDublicates)
+            Print(lDublicateNumbers);
+            Print(lDublicateText);
+
+            Console.ReadLine();
+        }
+
+        static void Print<T>(IEnumerable<T> set)
+        {
+            foreach (T item in set)
             {
                 Console.WriteLine(item);
             }
-
-            Console.ReadLine();
         }
     }
 }
