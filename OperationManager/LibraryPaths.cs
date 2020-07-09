4675056140aa0d321c66ca5bc2d4a5224f2385ac
arthur-avetikyan <arthuravetikyan@gmail.com>
2020-07-09 17:25:10 +0400

    organizing

--- OperationManager/LibraryPaths.cs
+++ OperationManager/LibraryPaths.cs
@@ -4,11 +4,11 @@ namespace OperationManager
 {
     public class LibraryPaths
     {
-        private const string _addition = "Addition.dll";
+        private const string _addition = "Add.dll";
         private const string _subtraction = "Subtraction.dll";
         private const string _multiplication = "Multiplication.dll";
         private const string _division = "Division.dll";
-        private const string _operation = "Operation.dll";
+        private const string _operation = "IOperation.dll";
 
         public const string _pluginDirectory = @"C:\Users\arthu\source\repos\Tasks\Plugins";
         public Dictionary<int, string> availablePlugins = new Dictionary<int, string>
