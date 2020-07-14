354221776477aa55926edac86c8eef08321e3364
arthur-avetikyan <arthuravetikyan@gmail.com>
2020-07-14 10:29:40 +0400

    Strategy

---
+++ OperationManager/OperationPerformer.cs
@@ -0,0 +1,35 @@
+﻿using Operation;
+using OperationManager.UI;
+using System;
+
+namespace OperationManager
+{
+    public class OperationPerformer
+    {
+        private IOperation _operation;
+        double[] _numbers;
+
+        public OperationPerformer(IOperation operation, double[] numbers)
+        {
+            _operation = operation;
+            _numbers = numbers;
+        }
+
+        public void PerformOperation()
+        {
+            double lResult = _operation.Operate(_numbers);
+            DisplayOutput(lResult);
+        }
+
+        private void DisplayOutput(double result)
+        {
+            Console.Write($"{Environment.NewLine} {UITexts.ResultMessage} {_numbers[0]} ");
+            for (int i = 1; i < _numbers.Length; i++)
+            {
+                Console.Write($"{_operation.OperationRepresentation} {_numbers[i]} ");
+            }
+            Console.Write($"= {result}");
+        }
+
+    }
+}
