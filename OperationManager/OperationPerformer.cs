50c25baf49bab584e7c35dc1ca95a01cfcdb2b13
arthur-avetikyan <arthuravetikyan@gmail.com>
2020-07-14 17:20:55 +0400

    Try

--- OperationManager/OperationPerformer.cs
+++ OperationManager/OperationPerformer.cs
@@ -12,12 +12,20 @@ namespace OperationManager
             _iOperationResolver = iOperationResolver;
         }
 
-        public void PerformOperation(string option, double[] numbers)
+        public void PerformOperation(string option, params double[] numbers)
         {
             double lResult = _iOperationResolver.Resolve(option).Operate(numbers);
-            DisplayOutput(lResult);
+            DisplayOutput(option, lResult, numbers);
         }
 
-        private void DisplayOutput(double result) => Console.Write($"{UITexts.ResultMessage} {result} ");
+        private void DisplayOutput(string option, double result, params double[] numbers)
+        {
+            Console.Write($"{Environment.NewLine} {UITexts.ResultMessage} {numbers[0]} ");
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                Console.Write($"{option} {numbers[i]} ");
+            }
+            Console.Write($"= {result}");
+        }
     }
 }
--- OperationManager/Program.cs
+++ OperationManager/Program.cs
@@ -9,7 +9,12 @@ namespace OperationManager
         {
             OperationResolver lResolver = new OperationResolver();
             OperationPerformer lOperationPerformer = new OperationPerformer(lResolver);
-            lOperationPerformer.PerformOperation(UIHandler.GetOperation(lResolver.Operations), UIHandler.GetNumbersInput());
+        //    lOperationPerformer.PerformOperation(UIHandler.GetOperation(lResolver.Operations), UIHandler.GetNumbersInput());
+
+            lOperationPerformer.PerformOperation("Add", 1, 5, 6);
+            lOperationPerformer.PerformOperation("*", 1, 5, 6);
+            lOperationPerformer.PerformOperation("-", 1, 5, 6);
+            lOperationPerformer.PerformOperation("/", 1, 5, 6);
 
             Console.ReadLine();
         }
